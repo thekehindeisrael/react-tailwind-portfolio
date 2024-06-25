a913b57b48aba1f70bad89f92147aa2c465a67bc
Kehinde Joseph <118644955+thekehindeisrael@users.noreply.github.com>
2024-06-25 15:24:00 +0100

    Changed Osuji to Okuji

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ function HeroSection() {
 					Welcome To My Personal Portfolio
 				</h1>
 				<p className="text-md md:text-xl max-w-2x1 mb-3 text-gray-600 dark:text-gray-300">
-					I'm Awa Osuji Michael and I am a frontend developer with a passion for creating visually appealing and user-friendly websites. With 2 years of experience in the field, I have honed my skills in HTML, CSS, JavaScript and React to bring my clients ideas to life. My goal is to always deliver high-quality and responsive websites that meet the needs of both the user and the client. Whether it's a simple brochure website or a complex e-commerce platform, I am committed to creating a seamless and enjoyable experience for everyone who visits.{' '}
+					I'm Awa Okuji Michael and I am a frontend developer with a passion for creating visually appealing and user-friendly websites. With 2 years of experience in the field, I have honed my skills in HTML, CSS, JavaScript and React to bring my clients ideas to life. My goal is to always deliver high-quality and responsive websites that meet the needs of both the user and the client. Whether it's a simple brochure website or a complex e-commerce platform, I am committed to creating a seamless and enjoyable experience for everyone who visits.{' '}
 				</p>
 				<a
 					href="#works"
